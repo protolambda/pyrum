f9dccd7ef88e8d42c9551742568d1822342bc3d6
protolambda <proto@protolambda.com>
2020-03-27 04:47:13 +0100

    update to use trio, big refactor, use send/receive streams

--- pyrum.py
+++ pyrum.py
@@ -1,54 +1,122 @@
-import asyncio
 import json
 
-from typing import List, Dict, Union, AsyncGenerator, Any, Callable, Coroutine
-from asyncio import Future, Queue as AsyncQueue
-from asyncio.subprocess import Process
+from typing import List, Dict, Any, Callable, Tuple
+import signal
+import trio
+import subprocess
 
+# After a MAX_MSG_BUFFER_SIZE unprocessed messages, there will be back-pressure on the producer.
+MAX_MSG_BUFFER_SIZE = 1000
 
 CallID = str
+_RECEIVE_SIZE = 4096  # pretty arbitrary
+
+
+# From Trio GitHub issue: https://github.com/python-trio/trio/issues/796#issuecomment-471428274
+class TerminatedFrameReceiver:
+    """Parse frames out of a Trio stream, where each frame is terminated by a
+    fixed byte sequence.
+
+    For example, you can parse newline-terminated lines by setting the
+    terminator to b"\n".
+
+    This uses some tricks to protect against denial of service attacks:
+
+    - It puts a limit on the maximum frame size, to avoid memory overflow; you
+    might want to adjust the limit for your situation.
+
+    - It uses some algorithmic trickiness to avoid "slow loris" attacks. All
+      algorithms are amortized O(n) in the length of the input.
+
+    """
+    def __init__(self, stream, terminator, max_frame_length=16384):
+        self.stream = stream
+        self.terminator = terminator
+        self.max_frame_length = max_frame_length
+        self._buf = bytearray()
+        self._next_find_idx = 0
+
+    async def receive(self):
+        while True:
+            terminator_idx = self._buf.find(
+                self.terminator, self._next_find_idx
+            )
+            if terminator_idx < 0:
+                # no terminator found
+                if len(self._buf) > self.max_frame_length:
+                    raise ValueError("frame too long")
+                # next time, start the search where this one left off
+                self._next_find_idx = max(0, len(self._buf) - len(self.terminator) + 1)
+                # add some more data, then loop around
+                more_data = await self.stream.receive_some(_RECEIVE_SIZE)
+                if more_data == b"":
+                    if self._buf:
+                        raise ValueError("incomplete frame")
+                    raise trio.EndOfChannel
+                self._buf += more_data
+            else:
+                # terminator found in buf, so extract the frame
+                frame = self._buf[:terminator_idx]
+                # Update the buffer in place, to take advantage of bytearray's
+                # optimized delete-from-beginning feature.
+                del self._buf[:terminator_idx+len(self.terminator)]
+                # next time, start the search from the beginning
+                self._next_find_idx = 0
+                return frame
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        try:
+            return await self.receive()
+        except trio.EndOfChannel:
+            raise StopAsyncIteration
 
 
 class Rumor(object):
     calls: Dict[CallID, "Call"]
     _unique_call_id_counter: int
+    _cmd: str
     _debug: bool
-    rumor_process: Process
-    _closed: Future
+    _nursery: trio.Nursery
+    _exit_nursery: Any
+    rumor_process: trio.Process
+    _to_rumor: trio.MemorySendChannel
 
     actors: Dict[str, "Actor"]
 
-    def __init__(self, debug: bool = False):
+    def __init__(self, cmd: str = 'rumor', debug: bool = False):
         self._debug = debug
-        self._closed = Future()
+        self._cmd = cmd
         self.calls = {}
         self.actors = {}
         self._unique_call_id_counter = 0
 
-    async def start(self, cmd: str = 'rumor'):
-        self.rumor_process = await asyncio.create_subprocess_shell(
-            cmd,
-            stdin=asyncio.subprocess.PIPE,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
-        loop = asyncio.get_event_loop()
+    async def __aenter__(self) -> "Rumor":
+        nursery_mng = trio.open_nursery()
+        self._nursery = await nursery_mng.__aenter__()
+        self._exit_nursery = nursery_mng.__aexit__
+
+        self.rumor_process = await trio.open_process(
+            self._cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+        )
+
+        self._to_rumor, _for_rumor = trio.open_memory_channel(20)  # buffer information to be sent to rumor
+
+        async def write_loop():
+            async for line in _for_rumor:
+                if self._debug:
+                    print('Sending line to Rumor:' + str(line))
+                inp: trio.abc.SendStream = self.rumor_process.stdin
+                await inp.send_all((line + '\n').encode())
 
         async def read_loop():
-            while True:
-                next_line_fut = asyncio.ensure_future(self.rumor_process.stdout.readline())
-                await asyncio.wait([next_line_fut, self._closed], return_when=asyncio.FIRST_COMPLETED)
-                if self._closed.done():
-                    break
-                line = next_line_fut.result()
-                if line == b'':
-                    if self._debug:
-                        print("Closing Rumor debug loop")
-                    return
-
-                line = line.decode('utf-8')
-                if line.endswith('\n'):
-                    line = line[:-1]
-
+            async for line in TerminatedFrameReceiver(self.rumor_process.stdout, b'\n'):
                 if self._debug:
                     print('Received line from Rumor:' + str(line))
 
@@ -66,62 +134,37 @@ class Rumor(object):
                             continue
 
                         call: Call = self.calls[call_id]
-                        await call.on_entry(entry)
-                        if call.ok.done() or call.ok.cancelled():
+                        if call.ok.is_set():
                             # call is over, remove it
                             del self.calls[call_id]
-
-                # find the corresponding actor, pass on the event.
-                if 'actor' in entry:
-                    actor_name = entry['actor']
-                    if actor_name not in self.actors:
-                        self.actors[actor_name] = Actor(self, actor_name)
-
-                    actor = self.actors[actor_name]
-                    await actor.on_entry(entry)
+                        else:
+                            await call.send_ch.send(entry)
 
         async def debug_loop():
-            while True:
-                next_line_fut = asyncio.ensure_future(self.rumor_process.stderr.readline())
-                await asyncio.wait([next_line_fut, self._closed], return_when=asyncio.FIRST_COMPLETED)
-                if self._closed.done():
-                    break
-                line = next_line_fut.result()
-                if line == b'':
-                    print("Closing Rumor debug loop")
-                    return
-                line = line.decode('utf-8')
-                if line.endswith('\n'):
-                    line = line[:-1]
+            async for line in TerminatedFrameReceiver(self.rumor_process.stderr, b'\n'):
                 print(f"ERROR from Rumor: '{line}'")
 
         if self.debug:
-            loop.create_task(debug_loop())
+            self._nursery.start_soon(debug_loop)
 
-        loop.create_task(read_loop())
+        self._nursery.start_soon(write_loop)
+        self._nursery.start_soon(read_loop)
 
-    async def stop(self):
-        # Clean up, read-loops should stop
-        self._closed.set_result(None)
-        # Ask it nicely to stop, then wait for it to complete
-        inp = self.rumor_process.stdin
-        inp.write('exit\n'.encode())
-        await inp.drain()
-        await self.rumor_process.wait()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self._nursery.cancel_scope.cancel()
+        # Stop all tasks that use the process
+        await self._exit_nursery(exc_type, exc_val, exc_tb)
+        # Ask it nicely to stop
+        self.rumor_process.send_signal(signal.SIGINT)
+        # Wait for it to complete
+        await self.rumor_process.aclose()
 
     def actor(self, name: str) -> "Actor":
         if name not in self.actors:
             self.actors[name] = Actor(self, name)
         return self.actors[name]
-
-    async def _send_to_rumor_process(self, line):
-        if self._closed.done():
-            raise Exception("Rumor is closed")
-        inp = self.rumor_process.stdin
-        if self._debug:
-            print(f"Sending command to Rumor: '{line}'")
-        inp.write((line + '\n').encode())
-        await inp.drain()
 
     def make_call(self, actor: str, args: List[str]) -> "Call":
         # Get a unique call ID
@@ -132,12 +175,12 @@ class Rumor(object):
         call = Call(self, call_id, cmd)
         self.calls[call_id] = call
         # Send the actual command, with call ID, to Rumor
-        asyncio.create_task(self._send_to_rumor_process(f'{call_id}> {actor}: bg {cmd}'))
+        self._nursery.start_soon(self._to_rumor.send, f'{call_id}> {actor}: bg {cmd}')
         return call
 
     async def cancel_call(self, call_id: CallID):
         # Send the actual command, with call ID, to Rumor
-        await self._send_to_rumor_process(f'{call_id}> cancel')
+        await self._to_rumor.send(f'{call_id}> cancel')
 
     # No actor, Rumor will just default to a default-actor.
     # But this is useful for commands that don't necessarily have any actor, e.g. debugging the contents of an ENR.
@@ -155,21 +198,9 @@ class Actor(object):
     def __init__(self, rumor: Rumor, name: str):
         self.rumor = rumor
         self.name = name
-        self.q = AsyncQueue()
 
     def __getattr__(self, item) -> "Cmd":
         return Cmd(self.rumor, self.name, [item])
-
-    async def on_entry(self, entry: Dict[str, Any]):
-        await self.q.put(entry)
-
-    async def logs(self) -> AsyncGenerator:
-        """Async generator to wait for and yield each log entry, including errors"""
-        while True:
-            v = await self.q.get()
-            if v is None:
-                break
-            yield v
 
 
 class Cmd(object):
@@ -194,107 +225,93 @@ class CallException(Exception):
 IGNORED_KEYs = ['actor', 'call_id', 'level', '@success', 'time']
 
 
+class AwaitableReadChannel(object):
+    recv: trio.MemoryReceiveChannel
+
+    def __init__(self, recv):
+        self.recv = recv
+
+    def __aiter__(self):
+        return self.recv.__aiter__()
+
+    def __await__(self):
+        return self.recv.receive().__await__()
+
+
 class Call(object):
     rumor: Rumor
     data: Dict[str, Any]  # merge of all data so far
-    _awaited_data: Dict[str, Future]  # one-time awaited specific keys
-    _queued_data: Dict[str, AsyncQueue]  # by specific key
-    q: AsyncQueue  # all logs
+    _awaited_data: Dict[str, Tuple[trio.MemorySendChannel, trio.MemoryReceiveChannel]]  # data that is expected
+    _send_ch: trio.MemorySendChannel
+    ok: trio.Event  # event when the call is finished
     call_id: CallID
     cmd: str
-    # Future to wait for command to complete, future raises exception if any error entry makes it first.
-    ok: Future
+    all: AwaitableReadChannel  # to listen to all log result entries
+    err: trio.Event  # event when the call encounters any error (the listener may decide to ignore)
 
     def __init__(self, rumor: Rumor, call_id: CallID, cmd: str):
         self.rumor = rumor
         self.data = {}
         self._awaited_data = {}
-        self._queued_data = {}
         self.call_id = call_id
         self.cmd = cmd
-        self.ok = Future()
-        self.q = AsyncQueue()
+        self.ok = trio.Event()
+        self.err = trio.Event()
+        recv_ch: trio.MemoryReceiveChannel
+        self.send_ch, recv_ch = trio.open_memory_channel(MAX_MSG_BUFFER_SIZE)
+        send_all: trio.MemorySendChannel
+        send_all, recv_all = trio.open_memory_channel(MAX_MSG_BUFFER_SIZE)
+        self.all = AwaitableReadChannel(recv_all)
 
-    async def on_entry(self, entry: Dict[str, Any]):
-        # Log the entry, if call is being monitored
-        await self.q.put(entry)
+        async def process_messages():
+            async for entry in recv_ch:
+                if entry['level'] == 'error':
+                    self.err.set()
+                if '@success' in entry:
+                    self.ok.set()
+                    await send_all.aclose()
+                    await recv_ch.aclose()
+                    for (sch, rch) in self._awaited_data.values():
+                        await sch.aclose()
+                        await rch.aclose()
+                    return
+                for k, v in entry.items():
+                    # If the data is being awaited, then push it into the channel
+                    if k not in self._awaited_data:
+                        self._awaited_data[k] = trio.open_memory_channel(MAX_MSG_BUFFER_SIZE)
 
-        # Don't process the contents of the final closing message,
-        # it does not contain anything useful
-        if '@success' not in entry:
-            # Merge in new result data, overwrite any previous data
-            for k, v in entry.items():
-                if k in IGNORED_KEYs:
-                    continue
-                self.data[k] = v
+                    send_ch, _ = self._awaited_data[k]
+                    await send_ch.send(v)
+                    # Merge in new result data, overwrite any previous data
+                    if k not in IGNORED_KEYs:
+                        self.data[k] = v
+                await send_all.send(entry)
 
-                # Add the data to a queue, so it can be listened for, even after the fact
-                if k not in self._queued_data:
-                    self._queued_data[k] = AsyncQueue()
-                await self._queued_data[k].put(v)
-
-                # If the data is being awaited, then finish the future
-                if k in self._awaited_data:
-                    f = self._awaited_data[k]
-                    if not f.done():  # If we didn't have something complete it earlier
-                        f.set_result(v)
-
-        # Complete call future with exception
-        if entry['level'] == 'error':
-            await self._on_finish()
-            self.ok.set_exception(CallException(self.call_id, entry))
-            return
-        # Complete call future as normal
-        if '@success' in entry:  # special key, present in last log, after command completes
-            await self._on_finish()
-            self.ok.set_result(self.data)  # complete with the full set of latest merged data.
-            return
-
-    async def _on_finish(self):
-        """Cleans up the remaining things before the call truly completes"""
-        # Close all unfinished data futures with an exception
-        for k, v in self._awaited_data.items():
-            if not v.done():
-                v.set_exception(KeyError('Never got data for key: %s' % k))
-        # Close all queues by adding None sentinel.
-        for k, q in self._queued_data.items():
-            await q.put(None)
-
-    async def logs(self) -> AsyncGenerator:
-        """Async generator to wait for and yield each log entry, including errors"""
-        while True:
-            v = await self.q.get()
-            yield v
+        rumor._nursery.start_soon(process_messages)
 
     async def cancel(self):
         await self.rumor.cancel_call(self.call_id)
 
-    def __getattr__(self, item) -> Callable[[], Union[Coroutine, AsyncGenerator]]:
+    async def _result(self) -> Dict[str, Any]:
+        await self.ok.wait()
+        return self.data
+
+    def __await__(self):
+        return self._result().__await__()
+
+    def __getattr__(self, item) -> Callable[[], AwaitableReadChannel]:
         if not isinstance(item, str):
             raise AttributeError(f"cannot handle non-str attributes: {item}")
 
-        if item.startswith('listen_'):
-            item = item[len('listen_'):]
+        def attr_ch() -> AwaitableReadChannel:
+            # check if we already have a channel open for this item
+            if item in self._awaited_data:
+                _, recv = self._awaited_data[item]
+                return AwaitableReadChannel(recv)
 
-            async def attr_async_gen():
-                if item not in self._queued_data:
-                    self._queued_data[item] = AsyncQueue()
-                q = self._queued_data[item]
-                while True:
-                    v = await q.get()
-                    if v is None:  # Special sentinel to effectively stop the queue
-                        break
-                    yield v
+            # Otherwise, register that we like to see the data and return the listen channel for it
+            send, recv = trio.open_memory_channel(MAX_MSG_BUFFER_SIZE)
+            self._awaited_data[item] = (send, recv)
+            return AwaitableReadChannel(recv)
 
-            return attr_async_gen
-        else:
-            async def attr_fut():
-                # If the data is already here, then just return it
-                if item in self.data:
-                    return self.data[item]
-                # Otherwise, register that we like to see the data
-                fut = self._awaited_data[item] = Future()
-                # And then wait for and return the result
-                return await fut
-
-            return attr_fut
+        return attr_ch
